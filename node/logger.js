6ea25146cf17c31900c155c23d6f08a59e9d599f
David <111139186+TurtlePrograms@users.noreply.github.com>
2024-10-28 19:05:26 +0100

    updated logger to use winston

--- node/logger.js
+++ node/logger.js
@@ -1,17 +1,34 @@
-const fs = require('fs');
+const { createLogger, format, transports } = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
 const path = require('path');
 
-const logFilePath = path.join(__dirname, 'api.log');
+const logFilePath = path.join(__dirname, 'api-%DATE%.log');
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
+    ),
+    transports: [
+        new DailyRotateFile({
+            filename: logFilePath,
+            datePattern: 'YYYY-MM-DD',
+            maxSize: '20m',
+            maxFiles: '14d'
+        }),
+        new transports.Console()
+    ]
+});
 
 function formatParams(params) {
     return Object.entries(params).map(([key, value]) => `${key}: ${value}`).join(', ');
 }
 
 function logFunctionCall(functionName, params) {
-    const timestamp = new Date().toISOString();
     const formattedParams = formatParams(params);
-    const logMessage = `[${timestamp}] Function: ${functionName}, Params: ${formattedParams}\n`;
-    fs.appendFileSync(logFilePath, logMessage);
+    const logMessage = `Function: ${functionName}, Params: ${formattedParams}`;
+    logger.info(logMessage);
 }
 
 module.exports = logFunctionCall;

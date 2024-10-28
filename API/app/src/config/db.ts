9b257a7f73a75acd421756e0673dce73d949b26e
Kevin Dohmen <kevindohmen06@gmail.com>
2024-10-28 21:08:17 +0100

    attempt at full docker stuff

--- API/app/src/config/db.ts
+++ API/app/src/config/db.ts
@@ -3,15 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(process.env.DB_HOST);
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 export default db;
